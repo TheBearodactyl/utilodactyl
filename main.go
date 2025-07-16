50944af1031b888a1c37e1d2776c15a4b111364c
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-07-16 18:45:19 -0500

    clear terminal window on run

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"utilodactyl/actions"
 	"utilodactyl/models"
 
@@ -8,6 +9,7 @@ import (
 )
 
 func main() {
+	fmt.Print("\033[H\033[2J")
 	arg.MustParse(&models.Cli)
 	err := actions.App()
 	if err != nil {
